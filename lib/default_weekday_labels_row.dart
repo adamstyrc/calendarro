f54fb909b233ec2eab3186fddf3dbbe4041f344c
SebastienBtr <sebastien29520@gmail.com>
2020-01-03 18:45:53 +0100

    Use DateFormat to get the weekday labels
    By using DateFormat the labels will change according to the device language

--- lib/default_weekday_labels_row.dart
+++ lib/default_weekday_labels_row.dart
@@ -1,17 +1,60 @@
 import 'package:flutter/widgets.dart';
 
 class CalendarroWeekdayLabelsView extends StatelessWidget {
+  final DateTime monday = DateTime(2020, 01, 06);
+  final DateTime tuesday = DateTime(2020, 01, 07);
+  final DateTime wednesday = DateTime(2020, 01, 08);
+  final DateTime thursday = DateTime(2020, 01, 09);
+  final DateTime friday = DateTime(2020, 01, 10);
+  final DateTime saturday = DateTime(2020, 01, 11);
+  final DateTime sunday = DateTime(2020, 01, 12);
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
-        Expanded(child: Text("Mon", textAlign: TextAlign.center)),
-        Expanded(child: Text("Tue", textAlign: TextAlign.center)),
-        Expanded(child: Text("Wed", textAlign: TextAlign.center)),
-        Expanded(child: Text("Thu", textAlign: TextAlign.center)),
-        Expanded(child: Text("Fri", textAlign: TextAlign.center)),
-        Expanded(child: Text("Sat", textAlign: TextAlign.center)),
-        Expanded(child: Text("Sun", textAlign: TextAlign.center)),
+        Expanded(
+          child: Text(
+            DateFormat('E').format(monday),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            DateFormat('E').format(tuesday),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            DateFormat('E').format(wednesday),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            DateFormat('E').format(thursday),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            DateFormat('E').format(friday),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            DateFormat('E').format(saturday),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            DateFormat('E').format(sunday),
+            textAlign: TextAlign.center,
+          ),
+        ),
       ],
     );
   }
